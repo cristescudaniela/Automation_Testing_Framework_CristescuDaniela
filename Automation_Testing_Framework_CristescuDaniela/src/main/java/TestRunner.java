f158aff2aecc35a15b015843c026758bbe32bf58
cristescudaniela <cristescudaniela14@gmail.com>
2023-03-22 17:09:26 +0200

    Getting familiar with how to set up and configure the web driver Selenium.

--- Automation_Testing_Framework_CristescuDaniela/src/main/java/TestRunner.java
+++ Automation_Testing_Framework_CristescuDaniela/src/main/java/TestRunner.java
@@ -1,5 +1,8 @@
+import managers.WebDriverManager;
+
 public class TestRunner {
     public static void main(String[] args) {
-
+        WebDriverManager webDriverManager = new WebDriverManager("CHROME");
+        webDriverManager.getDriver().get("https://www.google.com/");
     }
 }
